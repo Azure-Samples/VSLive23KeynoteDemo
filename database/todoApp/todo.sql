8fa94c9d464af253fee35ee49a71b0e8fb700996
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2023-03-15 08:21:14 -1000

    Update todo.sql

--- database/todoApp/todo.sql
+++ database/todoApp/todo.sql
@@ -1,5 +1,5 @@
 CREATE TABLE [dbo].[todos] (
-    [Id]        INT             IDENTITY (1, 1) NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
+    [id]        INT             IDENTITY (1, 1) NOT NULL PRIMARY KEY CLUSTERED ([Id] ASC),
     [title]     NVARCHAR (1000) NOT NULL,
     [completed] BIT             CONSTRAINT [DEFAULT_todo_completed] DEFAULT 0 NOT NULL,
     [owner_id]  NVARCHAR (200)  CONSTRAINT [DEFAULT_todo_owner_id] DEFAULT 'public' NOT NULL,
